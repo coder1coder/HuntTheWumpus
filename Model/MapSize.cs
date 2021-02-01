dd50a720706ab28d883f529ce1946aaf3212a6b7
coder1coder <13835906+coder1coder@users.noreply.github.com>
2021-02-01 16:49:59 +0300

    Add MapSize class

---
+++ Model/MapSize.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleGame.Model
+{
+    class MapSize
+    {
+        public byte Width { get; set; }
+        public byte Height { get; set; }
+
+        public MapSize(byte width, byte height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
