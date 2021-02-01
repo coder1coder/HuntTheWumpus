8951486fff8b96a5abfb851d756fed5189901c05
coder1coder <13835906+coder1coder@users.noreply.github.com>
2021-02-01 16:51:23 +0300

    remove all methods from Player class

--- Model/Player.cs
+++ Model/Player.cs
@@ -6,43 +6,5 @@
         {
             Symbol = "@";
         }
-
-        public void Move(Direction direction)
-        {
-            switch (direction)
-            {
-                case Direction.UP:
-                    Position.Y -= 1;
-                    break;
-                case Direction.DOWN:
-                    Position.Y += 1;
-                    break;
-                case Direction.LEFT:
-                    Position.X -= 1;
-                    break;
-                case Direction.RIGHT:
-                    Position.X += 1;
-                    break;
-            }
-        }
-
-        public void Shot(Direction direction)
-        {
-            //switch(direction)
-            //{
-            //    case Direction.UP:
-            //        Position.Y -= 1;
-            //        break;
-            //    case Direction.DOWN:
-            //        Position.Y += 1;
-            //        break;
-            //    case Direction.LEFT:
-            //        Position.X -= 1;
-            //        break;
-            //    case Direction.RIGHT:
-            //        Position.X += 1;
-            //        break;
-            //}
-        }
     }
 }
