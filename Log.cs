a1fe56d77e26107c802dc4b188a675918bcaf2cf
Михаил Кодер <coder1coder@outlook.com>
2021-02-01 23:29:58 +0300

    Create Log class

---
+++ Log.cs
@@ -0,0 +1,17 @@
+﻿namespace HuntTheWumpus
+{
+    internal class Log
+    {
+        private string _log = "";
+
+        public void Add(string message)
+        {
+            _log = message + "\r\n" + _log;
+        }
+
+        public override string ToString()
+        {
+            return _log;
+        }
+    }
+}
