2225c5905926d0ea7899da3e78d552eaf4e68fde
coder1coder <13835906+coder1coder@users.noreply.github.com>
2021-02-01 16:53:15 +0300

    Change Map class
    + add method MoveUnit
    + remove SetUnitAtPosition
    + change ClearUnitAtPosition
    + some changes for new type MapSize

--- Model/Map.cs
+++ Model/Map.cs
@@ -4,31 +4,78 @@ namespace ConsoleGame.Model
 {
     class Map
     {
-        private object[,] _map;
+        private readonly Unit[,] _map;
+
+        public MapSize Size { get; private set; }
 
         public Map(byte width, byte height)
         {
-            _map = new object[(height < 9) ? 9 : height, (width < 9) ? 9 : width];
-        }
-
-        public void SetUnitAtPosition(Unit unit, Position position)
-        {
-            _map[position.Y, position.X] = unit;
+            _map = new Unit[(height < 9) ? 9 : height, (width < 9) ? 9 : width];
+            Size = new MapSize((byte)_map.GetLength(1), (byte)_map.GetLength(0));
         }
 
         internal void ClearUnitAtPosition(Position position)
         {
-            _map[position.Y, position.X] = null;
+            ClearUnitAtPosition(position.X, position.Y);
         }
 
-        internal void ClearUnitAtPosition(byte x, byte y)
+        internal void ClearUnitAtPosition(int x, int y)
         {
             _map[y, x] = null;
         }
 
-        public Unit GetUnitAtPosition(byte x, byte y)
+        internal Unit GetUnitAtPosition(int x, int y)
         {
+            return _map[y, x] ?? null;
+        }
 
+        internal Unit GetUnitAtPosition(Position position)
+        {
+            return GetUnitAtPosition(position.X, position.Y);
+        }
+
+        internal Unit MoveUnit(Unit unit, Unit.Direction direction)
+        {
+            ClearUnitAtPosition(unit.Position);
+
+            switch(direction)
+            {
+                case Unit.Direction.UP:
+                    if (unit.Position.Y > 0)
+                        unit.Position.Y -= 1;
+                    break;
+                case Unit.Direction.DOWN:
+                    if (unit.Position.Y < Size.Height - 1)
+                        unit.Position.Y += 1;
+                    break;
+                case Unit.Direction.LEFT:
+                    if (unit.Position.X > 0)
+                        unit.Position.X -= 1;
+                    break;
+                case Unit.Direction.RIGHT:
+                    if (unit.Position.X < Size.Width - 1)
+                        unit.Position.X += 1;
+                    break;
+            }
+
+            _map[unit.Position.Y, unit.Position.X] = unit;
+            return unit;
+        }
+        internal Unit MoveUnit(Unit unit, Position position)
+        {
+            ClearUnitAtPosition(unit.Position);
+            unit.Position = position;
+
+            _map[unit.Position.Y, unit.Position.X] = unit;
+            return unit;
+        }
+        internal Unit MoveUnit(Unit unit, int x, int y)
+        {
+            ClearUnitAtPosition(unit.Position.X, unit.Position.Y);
+            unit.Position.X = x;
+            unit.Position.Y = y;
+            _map[y, x] = unit;
+            return unit;
         }
     }
 }
